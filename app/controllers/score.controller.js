44386d29eb860371a19e7ecebe1b85fce731387c
2020mt93666 <2020mt93666@wilp.bits-pilani.ac.in>
2022-04-03 12:54:26 +0530

    Made API asynchronous

--- app/controllers/score.controller.js
+++ app/controllers/score.controller.js
@@ -2,10 +2,12 @@ const db = require("../models");
 const ScoreModel = db.scores;
 
 // Create and Save a new score
-exports.create = (req, res) => {
+exports.create = async (req, res, next) => {
   // Validate request
   if (!req.body.title) {
-    res.status(400).send({ message: "Content can not be empty!" });
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
     return;
   }
 
@@ -16,56 +18,57 @@ exports.create = (req, res) => {
     published: req.body.published ? req.body.published : false
   });
 
-  // Save score in the database
-  score
-    .save(score)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the score."
-      });
+  try {
+    // Save score in the database
+    const data = await score.save();
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the score."
     });
+  }
 };
 
 // Retrieve all scores from the database.
-exports.findAll = (req, res) => {
+exports.findAll = async (req, res) => {
   const title = req.query.title;
-  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+  var condition = title ? {
+    title: {
+      $regex: new RegExp(title),
+      $options: "i"
+    }
+  } : {};
 
-  ScoreModel.find(condition)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving scores."
-      });
+  try {
+    const data = await ScoreModel.find(condition);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving scores."
     });
+  }
 };
 
 // Find a single score with an id
-exports.findOne = (req, res) => {
+exports.findOne = async (req, res) => {
   const id = req.params.id;
 
-  ScoreModel.findById(id)
-    .then(data => {
-      if (!data)
-        res.status(404).send({ message: "Not found score with id " + id });
-      else res.send(data);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .send({ message: "Error retrieving score with id=" + id });
+  try {
+    const data = await ScoreModel.findById(id);
+    if (!data)
+      res.status(404).send({
+        message: "Not found score with id " + id
+      });
+    else res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: "Error retrieving score with id=" + id
     });
+  }
 };
 
 // Update a score by the id in the request
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   if (!req.body) {
     return res.status(400).send({
       message: "Data to update can not be empty!"
@@ -74,40 +77,44 @@ exports.update = (req, res) => {
 
   const id = req.params.id;
 
-  ScoreModel.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then(data => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot update score with id=${id}. Maybe score was not found!`
-        });
-      } else res.send({ message: "score was updated successfully." });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating score with id=" + id
-      });
+  try {
+    const data = await ScoreModel.findByIdAndUpdate(id, req.body, {
+      useFindAndModify: false
     });
+    if (!data) {
+      res.status(404).send({
+        message: `Cannot update score with id=${id}. Maybe score was not found!`
+      });
+    } else res.send({
+      message: "score was updated successfully."
+    });
+  } catch (err) {
+    res.status(500).send({
+      message: "Error updating score with id=" + id
+    });
+  }
 };
 
 // Delete a score with the specified id in the request
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
   const id = req.params.id;
 
-  ScoreModel.findByIdAndRemove(id, { useFindAndModify: false })
-    .then(data => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot delete score with id=${id}. Maybe score was not found!`
-        });
-      } else {
-        res.send({
-          message: "score was deleted successfully!"
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Could not delete score with id=" + id
-      });
+  try {
+    const data = await ScoreModel.findByIdAndRemove(id, {
+      useFindAndModify: false
     });
+    if (!data) {
+      res.status(404).send({
+        message: `Cannot delete score with id=${id}. Maybe score was not found!`
+      });
+    } else {
+      res.send({
+        message: "score was deleted successfully!"
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: "Could not delete score with id=" + id
+    });
+  }
 };
