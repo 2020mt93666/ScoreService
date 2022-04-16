af0e61c22f307ff60744ddadd4e23cf2be3a199e
2020mt93666 <2020mt93666@wilp.bits-pilani.ac.in>
2022-04-16 12:07:11 +0530

    Fixed issue for returning single data when searched with name. Implemented Intercommunication with player and match service.

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,7 +4,11 @@ const {
   DB_HOST,
   DB_PORT,
   DB_NAME,
+  MATCH_HOST,
+  PLAYER_HOST
 } = process.env;
 module.exports = {
-  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+  matchApiUrl: `http://${MATCH_HOST}:8080/api/matches`,
+  playerApiUrl: `http://${PLAYER_HOST}:8080/api/players`
 };
--- app/controllers/score.controller.js
+++ app/controllers/score.controller.js
@@ -1,4 +1,6 @@
 const db = require("../models");
+const config = require("../config/db.config");
+const axios = require("axios");
 const ScoreModel = db.scores;
 
 // Create and Save a new score
@@ -24,6 +26,8 @@ exports.create = async (req, res, next) => {
   try {
     // Save score in the database
     const data = await score.save();
+    await updateMatch(data);
+    await updatePlayer(data);
     res.send(data);
   } catch (err) {
     res.status(500).send({
@@ -32,19 +36,91 @@ exports.create = async (req, res, next) => {
   }
 };
 
+let updateMatch = async (score) => {
+  axios.get(config.matchApiUrl + "?name=" + score.match)
+    .then(function (response) {
+      // handle success
+      if (response.data) {
+        if (score.type == "Run") {
+          response.data.actual = response.data.actual + score.count
+          if (score.endOfMatch == true) {
+            if (response.data.target > response.data.actual)
+              response.data.status = response.data.teamA + " Won";
+            else if (response.data.target < response.data.actual)
+              response.data.status = response.data.teamB + " Won";
+            else
+              response.data.status = "Match Tied";
+          }
+          axios.put(config.matchApiUrl + "/" + response.data.id, {
+            actual : response.data.actual,
+            status : response.data.status
+          }).then(function (response) {
+            // handle success
+            if (response.data.message == "match was updated successfully.")
+              console.log("Success");
+            else
+              console.log(response.data.message);
+          }).catch(function (error) {
+            // handle error
+            console.log(error);
+          });
+        }
+      }
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+};
+
+let updatePlayer = async (score) => {
+  axios.get(config.playerApiUrl + "?name=" + score.player)
+    .then(function (response) {
+      // handle success
+      if (response.data) {
+        if (score.type == "Run")
+          response.data.runs = response.data.runs + score.count;
+        else if (score.type == "Wicket")
+          response.data.wickets = response.data.wickets + score.count;
+        else if (score.type == "Catch")
+          response.data.catches = response.data.catches + score.count;
+        axios.put(config.playerApiUrl + "/" + response.data.id, {
+          runs: response.data.runs,
+          wickets: response.data.wickets,
+          catches: response.data.catches
+        }).then(function (response) {
+          // handle success
+          if (response.data.message == "player was updated successfully.")
+            console.log("Success");
+          else
+            console.log(response.data.message);
+        }).catch(function (error) {
+          // handle error
+          console.log(error);
+        });
+      }
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+};
+
 // Retrieve all scores from the database.
 exports.findAll = async (req, res) => {
   const name = req.query.name;
   var condition = name ? {
-    name: {
-      $regex: new RegExp(name),
-      $options: "i"
-    }
+    name: name
   } : {};
 
   try {
-    const data = await ScoreModel.find(condition);
-    res.send(data);
+    if (name) {
+      const data = await ScoreModel.findOne(condition);
+      res.send(data);
+    } else {
+      const data = await ScoreModel.find(condition);
+      res.send(data);
+    }
   } catch (err) {
     res.status(500).send({
       message: err.message || "Some error occurred while retrieving scores."
